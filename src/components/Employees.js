6c4dcfb98749ae35383bdf4be0b9c5e3764f35e3
ANKU <ankitadixit1122@gmail.com>
2023-11-21 12:14:11 -0600

    new_commit

---
+++ src/components/Employees.js
@@ -0,0 +1,22 @@
+const Employees = [{
+    id :1,
+    name: 'John',
+    age: 31
+},
+{
+    id :2,
+    name: 'MIKE',
+    age: 34
+},
+{
+    id :3,
+    name: 'Peter',
+    age: 32
+},
+{
+    id :4,
+    name: 'Sally',
+    age: 36
+}]
+
+export default Employees;
